f72b0ab2453c6e324e49b3c0ae4e06f8e725a413
Yuan <allegoyuanpaolo@gmail.com>
2025-11-23 00:31:28 +0800

    Resolved AddStocks

--- src/modules/addStocks.cbl
+++ src/modules/addStocks.cbl
@@ -26,24 +26,59 @@
            01 Invstat pic xx.
            01 searchKey pic x(25).
            01 RestockAmount pic 9(4).
+           01 choice pic 99.
+
+            01 foundRecord. 
+               02 foundTable occurs 10 times.
+                   03 foundName pic x(25).
+                   03 foundStock pic Z,ZZ9.
+                   03 foundRestock pic x(25).   
+
+           01 foundCount pic 9(2) value 0.
+
         PROCEDURE DIVISION.
-           display "Enter I(tem name to restock: " no advancing
+           display "Enter Item name to restock: " no advancing
            accept searchKey
 
-           open i-o Inventory
-               call "openFileCheck" using Invstat
-               
-               move searchKey to itemName
-               read Inventory key is itemName
-                   not invalid key
-                       display "Item: " itemName " | Current stock: " itemStock
-                       display "Restock quantity: " no advancing
-                       accept  RestockAmount
+           call "testSearch" using  searchKey foundRecord foundCount
 
-                   invalid key
-                       
-               end-read
+           if foundCount not = 0
+               display spaces
+               display "Select item to restock:"
+               display "[Enter number] >" no advancing
+               accept choice
 
-           close Inventory
-       STOP RUN.
+               display spaces 
+               if choice not = 0
+                   display "Item         : " foundName(choice) "|"
+                   display "Current stock: " foundStock(choice)
+                   display spaces
+                   display "Restock Amount: " no advancing
+                   accept RestockAmount
+                   
+                   open i-o Inventory
+                   move foundName(choice) to itemName
+                       read Inventory key is itemName
+                           invalid key
+                               display "Update failed"
+                   
+                           not invalid key
+                               compute RestockAmount = RestockAmount + itemStock
+                               move RestockAmount to itemStock
+                               rewrite invRec
+                                   invalid key
+                                       display "Failure code: " Invstat
+                                   not invalid key
+                                        display "Updated: "
+                                        display itemName " | " itemStock
+                       end-read
+                   close Inventory
+
+               end-if
+
+           end-if
+           
+           
+
+       exit program.
  
--- src/modules/testSearch.cbl
+++ src/modules/testSearch.cbl
@@ -22,70 +22,84 @@
                    02 restockStatus pic x(25).
             WORKING-STORAGE SECTION.
             01 InvStat pic xx.
-            01 searchKey pic x(25).
 
             01 foundflag pic 9 value 0.
             01 eof pic x value 'n'.
 
-            01 foundRecord. 
+           01 foundITR pic 9(2) value 1.
+       
+           LINKAGE SECTION.
+           01 foundCount pic 9(2).
+
+              01 foundRecord. 
                02 foundTable occurs 10 times.
                    03 foundName pic x(25).
                    03 foundStock pic Z,ZZ9.
-                   03 foundRestock pic x(25).
-           
-           01 foundITR pic 9(2) value 0.
-        
-        PROCEDURE DIVISION.
-        display "Searchkey >" no advancing
-        accept searchKey
-        display "------------------------------"
+                   03 foundRestock pic x(25).      
+            01 searchKey pic x(25).
+
+        PROCEDURE DIVISION using searchKey foundRecord foundCount.
+       
+      *> display "Enter key to search: " no advancing
+      *> accept  searchKey
         move searchKey to itemName
         
         open i-o Inventory
           call "openFileCheck" using InvStat
 
           read Inventory key is itemName
+               *> Starts linear search if only partial key
                invalid key
+                   *> initialize the flags
                    move 'n' to eof
                    move 0 to foundflag
                    
                    perform  until eof = 'y'
-                       move 0 to foundflag
+                       move 0 to foundflag *> Resets foundFlag every iteration
                        read Inventory next record
                            at end
                                move 'y' to eof
                            not at end
+                               *> checks for matches and updates foundFlag if found
                                inspect itemName tallying foundflag
                                for all
                                searchKey(1:function length(function trim(searchKey)))
                                
-                           if foundflag > 0
-                               add 1 to foundITR
+                           if foundflag > 0 and foundITR <= 10
                                move itemName to foundName(foundITR)
                                move itemStock to foundStock(foundITR)
+                               
+                               add 1 to foundITR 
+                               add 1 to foundCount
                            end-if
                    end-perform
-                   if foundITR = 0
+                   
+                   *> Display how many results found
+                   if foundCount = 0
                        display "No records found matching: " searchKey
                    else
-                       display "Results found: " foundITR
+                       display "Results found: " foundCount
+                       display "=================="
                    end-if
 
-                   move 1 to foundITR
 
-                   perform until foundITR = 10
+                   *> Display results
+                   *> Reset foundITR to traverse
+                   move 1 to foundITR
+                   perform until foundITR > foundCount
                        if foundName(foundITR) not = spaces
-                           display "Result #" foundITR " | "
+                           display "Result " foundITR " | "
                                    foundName(foundITR) " | "
                                    foundStock(foundITR) " | "
-                           add 1 to foundITR
+                           *>add 1 to foundCount
+                           add 1 to foundITR *> traverse table
                        else
                            exit perform 
                        end-if
                    end-perform
                        
 
-
+               *> Found by index search
                not invalid key
                    display "Found: " itemName
                    display itemName " | " itemStock
