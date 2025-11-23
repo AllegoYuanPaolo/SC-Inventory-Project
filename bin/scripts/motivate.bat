225a6ce8050ed2f300f42b405f03a56b48e77fae
Yuan <allegoyuanpaolo@gmail.com>
2025-11-24 00:32:55 +0800

    added documentation, edited gitignore

--- bin/scripts/motivate.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-if exist "Python Utils\misc" (
-    pushd "Python Utils\misc"
-        python randomer.py
-            for /L %%f in (1, 1, 5) do ( echo count >> counter.txt )
-            start "" /B python motivator.py
-    popd
-)
