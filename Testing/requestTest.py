0b9db95521ea26828cf2db9929c21a9399d75a1e
Yuan <allegoyuanpaolo@gmail.com>
2025-12-19 11:14:11 +0800

    developing stock request module

--- Testing/requestTest.py
+++ Testing/requestTest.py
@@ -1,6 +1,36 @@
-requestList = []
+from datetime import datetime
+itemList = []
+
+requestID = 0
+requestItem = ""
+requestQuanity = ""
+
 
 requestor = input("Department requesting stocks: ")
-while True:
-    ...
+with open("record.txt", "a") as record:
+    while True:
+        
+        requestItem = input("Item: ")
+        requestQuanity = int(input("Quantity: "))
+        itemList.append((requestItem, requestQuanity))
+
+        date = datetime.now()    
+        requestTime = date.strftime("%H:%M")
+        requestDate = date.strftime("%Y-%m-%d")
+
+        contFlag = input("Continue? [y/n]>")
+        if contFlag == 'n':
+            break
+    
+    record.write(f"Requestor: {requestor}\n")
+    record.write(f"Request date: {requestDate} | {requestTime}\n")
+    record.write("Items: \n")
+    for entry in itemList:
+        item, qty = entry
+        record.write(f"- Item: {item} - {qty}\n")
+
+
+
+    
+            
     
--- src/modules/requestStocks.cbl
+++ src/modules/requestStocks.cbl
@@ -1,19 +1,22 @@
         IDENTIFICATION DIVISION.
-        PROGRAM-ID. resetRequests.
-        
-        environment division.
+        PROGRAM-ID. requestStocks.
+ 
+       environment division.
            input-output section.
                file-control.
                    select Requests
                        assign to "data/Requests.dat"
                        organization is indexed
                        access mode is dynamic
-                       record key is requestID.
-                  
+                       record key is requestID
+                       file status is ReqStat
+
+                   copy "Inventory".
 
         DATA DIVISION.
-           file section.
-              fd Requests.
+           FILE section.
+           copy "Inventory-rec".
+            fd Requests.
                    01 Requests-rec.
                        02 requestID pic 9(5).
                        02 requestItem pic x(25).
@@ -21,19 +24,17 @@
                        02 requestor pic x(25)
                        02 requestDate pic x(10).
                        02 requestTime pic x(8).
-                   
 
             WORKING-STORAGE SECTION.
-               
-
-
+               01 input-rec.
+                   02 input-ID pic 9(5).
+                   02 input-Item pic x(25).
+                   02 input-Quantity pic 9(4).
+                   02 input-Requestor pic x(25).
+                   02 input-Date pic x(10).
+                   02 input-Time pic x(8).
+                   
         PROCEDURE DIVISION.
-           display "Resetting reqeusts records. . ."     
            
-           open output Requests
-           close Requests
-    
-           display "Requests records reset!"
-        
        STOP RUN.
  
